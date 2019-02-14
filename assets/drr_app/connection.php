b317143656c0359025ea1e147b79083aaf84a5f8
Julieto L. Ompad <jlompad@ENTDSWD.LOCAL>
2019-02-14 13:03:30 +0800

    Initial commit

---
+++ assets/drr_app/connection.php
@@ -0,0 +1,7 @@
+<?php
+
+$host = "crg-db1-svr";
+
+$con = pg_connect("host=".$host." dbname=drrrou_db user=postgres password=dswd123$");
+
+?>
---
+++ assets/js/ip.js
@@ -0,0 +1,6 @@
+var uip = window.location.protocol + "//" + window.location.host;
+
+var serverip 	= uip+"/drrrou/pages/";
+var serverip2 	= uip+"/drrrou/dromic_master_excel/";
+var opcenip 	= uip+"/drrrou/welcome/";
+var base_url 	= uip+"/drrrou/";
